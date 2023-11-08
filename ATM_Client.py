48557fa8efd4e6ebb4d5ffdd3737b5d7eaab6031
ErveyG <erveygg@outlook.com>
2023-11-08 13:19:43 -0600

    Codigo jalando no mover

--- ATM_Client.py
+++ ATM_Client.py
@@ -9,87 +9,48 @@ def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
-    login_attempts = 0
     logged_in = False
     account_number = None
 
     try:
-        while login_attempts < 3:
+        while True:
             if not logged_in:
+                account_number = input("Ingrese el número de cuenta: ")
+                pin = input("Ingrese su PIN: ")
+                client_socket.send(f"login {account_number} {pin}".encode('utf-8'))
+                response = client_socket.recv(1024).decode('utf-8')
+                if response == "Login exitoso.":
+                    logged_in = True
+                else:
+                    print(response)
+            else:
                 print("Opciones:")
-                print("1. Iniciar sesión")
-                print("2. Crear una nueva cuenta")
+                print("1. Consultar saldo")
+                print("2. Retirar dinero")
+                print("3. Depositar dinero")
+                print("4. Salir")
+
                 choice = input("Seleccione una opción: ")
 
                 if choice == "1":
-                    account_number = input("Ingrese el número de cuenta: ")
-                    pin = input("Ingrese su PIN: ")
-                    data = f"login {account_number} {pin}"
-                    client_socket.send(data.encode('utf-8'))  # Codificar y enviar
+                    client_socket.send(f"balance {account_number}".encode('utf-8'))
                     response = client_socket.recv(1024).decode('utf-8')
                     print(response)
-                    print("------------------\n")
-                    if response == "Login exitoso.":
-                        logged_in = True
-                        login_attempts = 0
-                    else:
-                        login_attempts += 1
-                # ... (código para crear nueva cuenta)
+                elif choice == "2":
+                    amount = float(input("Ingrese la cantidad a retirar: "))
+                    client_socket.send(f"withdraw {account_number} {amount}".encode('utf-8'))
+                    response = client_socket.recv(1024).decode('utf-8')
+                    print(response)
+                elif choice == "3":
+                    amount = float(input("Ingrese la cantidad a depositar: "))
+                    client_socket.send(f"deposit {account_number} {amount}".encode('utf-8'))
+                    response = client_socket.recv(1024).decode('utf-8')
+                    print(response)
+                elif choice == "4":
+                    client_socket.send("exit".encode('utf-8'))
+                    break
                 else:
                     print("Opción no válida")
-
-        if login_attempts >= 3:
-            print("Demasiados intentos de inicio de sesión. Saliendo.")
-            return
-
-        while True:
-            if not logged_in:
-                print("Sesión cerrada.")
-                break
-
-            print("Opciones:")
-            print("1. Consultar saldo")
-            print("2. Retirar dinero")
-            print("3. Depositar dinero")
-            print("4. Transferir dinero")
-            print("5. Salir")
-
-            choice = input("Seleccione una opción: ")
-
-            if choice == "1":
-                data = f"balance {account_number}"
-                client_socket.send(data.encode('utf-8'))  # Codificar y enviar
-                response = client_socket.recv(1024).decode('utf-8')
-                print(response)
-                print("------------------\n")
-            elif choice == "2":
-                amount = float(input("Ingrese la cantidad a retirar: "))
-                data = f"withdraw {account_number} {amount}"
-                client_socket.send(data.encode('utf-8'))  # Codificar y enviar
-                response = client_socket.recv(1024).decode('utf-8')
-                print(response)
-                print("------------------\n")
-            elif choice == "3":
-                amount = float(input("Ingrese la cantidad a depositar: "))
-                data = f"deposit {account_number} {amount}"
-                client_socket.send(data.encode('utf-8'))  # Codificar y enviar
-                response = client_socket.recv(1024).decode('utf-8')
-                print(response)
-                print("------------------\n")
-            elif choice == "4":
-                receiver_account = input("Ingrese el número de cuenta de destino: ")
-                amount = float(input("Ingrese la cantidad a transferir: "))
-                data = f"transfer {account_number} {receiver_account} {amount}"
-                client_socket.send(data.encode('utf-8'))  # Codificar y enviar
-                response = client_socket.recv(1024).decode('utf-8')
-                print(response)
-                print("------------------\n")
-            elif choice == "5":
-                data = "exit"
-                client_socket.send(data.encode('utf-8'))  # Codificar y enviar
-                break
-            else:
-                print("Opción no válida")
 
     finally:
         client_socket.close()
--- ATM_Server.py
+++ ATM_Server.py
@@ -1,109 +1,99 @@
 import socket
 import threading
-import hashlib
-import datetime
 
 # Configuración del servidor
-host = '0.0.0.0'
+host = '127.0.0.1'
 port = 12345
 
-# Clave compartida para cifrado (en un entorno real, esto debe ser más seguro)
-shared_key = 'my_shared_key'
-
-# Archivo de registro
-log_file = "server_log.txt"
-
-# Datos de usuarios (cuenta: (pin, saldo))
+# Cargando datos de usuarios desde un archivo de texto (simplificado)
 users = {}
-# Leer los datos de usuario desde el archivo users.txt
-with open("users.txt", "r") as user_file:
-    for line in user_file:
-        account, pin, balance = line.strip().split()
-        users[account] = (pin, float(balance))
-
-# Función para cifrar datos
-def encrypt(data):
-    cipher = hashlib.sha256(shared_key.encode()).digest()
-    encrypted_data = ''.join([chr((ord(data[i]) + ord(cipher[i % len(cipher)])) % 256) for i in range(len(data))])
-    return encrypted_data
-
-# Función para descifrar datos
-def decrypt(data):
-    cipher = hashlib.sha256(shared_key.encode()).digest()
-    decrypted_data = ''.join([chr((ord(data[i]) - ord(cipher[i % len(cipher)])) % 256) for i in range(len(data))])
-    return decrypted_data
-
-# Función para escribir registros en el archivo de registro
-def log_event(event):
-    with open(log_file, "a") as file:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{timestamp} - {event}\n")
-
-# Función para autenticar al usuario leyendo datos desde el archivo users.txt
-def authenticate_user(account_number, pin):
-    if account_number in users and users[account_number][0] == pin:
-        return True
-    return False
-
-# Función para procesar transferencias
-def transfer_funds(sender_account, receiver_account, amount):
-    if sender_account in users and receiver_account in users:
-        sender_balance = users[sender_account][1]
-        if sender_balance >= amount:
-            # Realizar la transferencia
-            users[sender_account] = (users[sender_account][0], sender_balance - amount)
-            users[receiver_account] = (users[receiver_account][0], users[receiver_account][1] + amount)
-            return True
-    return False
+with open('users.txt', 'r') as file:
+    for line in file:
+        account_number, pin, balance = line.strip().split(',')
+        users[account_number] = (pin, float(balance))
 
 # Función para manejar la conexión con un cliente
 def handle_client(client_socket):
     try:
-        login_attempts = 0
-        logged_in = False
-        account_number = None
-
-        while login_attempts < 3:
-            data = decrypt(client_socket.recv(1024).decode('utf-8'))
+        while True:
+            data = client_socket.recv(1024).decode('utf-8')
             if not data:
                 break
 
             if data == "exit":
                 break
             elif data.startswith("login"):
-                # Autenticación del usuario
                 account_number, pin = data.split()[1], data.split()[2]
                 if authenticate_user(account_number, pin):
-                    client_socket.send(encrypt("Login exitoso."))
-                    login_attempts = 0
-                    logged_in = True
-                    log_event(f"Inicio de sesión exitoso para la cuenta {account_number}")
+                    client_socket.send("Login exitoso.".encode('utf-8'))
                 else:
-                    login_attempts += 1
-                    client_socket.send(encrypt("PIN incorrecto o cuenta inexistente."))
-                    log_event(f"Intento de inicio de sesión fallido para la cuenta {account_number}")
-            elif logged_in:
-                if data.startswith("transfer"):
-                    # Realizar transferencia
-                    sender_account, receiver_account, amount = data.split()[1], data.split()[2], float(data.split()[3])
-                    if transfer_funds(sender_account, receiver_account, amount):
-                        client_socket.send(encrypt(f"Transferencia exitosa. Nuevo saldo: {users[sender_account][1]}"))
-                        log_event(f"Transferencia de {amount} desde {sender_account} a {receiver_account}")
-                    else:
-                        client_socket.send(encrypt("Error en la transferencia. Verifique los datos."))
+                    client_socket.send("PIN incorrecto o cuenta inexistente.".encode('utf-8'))
+            elif data.startswith("balance"):
+                account_number = data.split()[1]
+                balance = get_balance(account_number)
+                client_socket.send(f"Saldo actual: ${balance}".encode('utf-8'))
+            elif data.startswith("withdraw"):
+                account_number, amount = data.split()[1], float(data.split()[2])
+                success, new_balance = withdraw(account_number, amount)
+                if success:
+                    client_socket.send(f"Retiro exitoso. Nuevo saldo: ${new_balance}".encode('utf-8'))
                 else:
-                    client_socket.send(encrypt("Comando no válido."))
+                    client_socket.send("Fondos insuficientes.".encode('utf-8'))
+            elif data.startswith("deposit"):
+                account_number, amount = data.split()[1], float(data.split()[2])
+                new_balance = deposit(account_number, amount)
+                client_socket.send(f"Depósito exitoso. Nuevo saldo: ${new_balance}".encode('utf-8'))
+            else:
+                client_socket.send("Comando no válido.".encode('utf-8'))
 
     finally:
         client_socket.close()
 
-# Configurar el servidor
+# Función para autenticar al usuario
+def authenticate_user(account_number, pin):
+    if account_number in users and users[account_number][0] == pin:
+        return True
+    return False
+
+# Función para obtener el saldo de un usuario
+def get_balance(account_number):
+    if account_number in users:
+        return users[account_number][1]
+    return 0
+
+# Función para realizar un retiro
+def withdraw(account_number, amount):
+    if account_number in users:
+        current_balance = users[account_number][1]
+        if current_balance >= amount:
+            new_balance = current_balance - amount
+            users[account_number] = (users[account_number][0], new_balance)
+            update_user_data()
+            return True, new_balance
+    return False, 0
+
+# Función para realizar un depósito
+def deposit(account_number, amount):
+    if account_number in users:
+        current_balance = users[account_number][1]
+        new_balance = current_balance + amount
+        users[account_number] = (users[account_number][0], new_balance)
+        update_user_data()
+        return new_balance
+
+# Función para actualizar los datos de usuario en el archivo de texto
+def update_user_data():
+    with open('users.txt', 'w') as file:
+        for account_number, (pin, balance) in users.items():
+            file.write(f"{account_number},{pin},{balance}\n")
+
+# Inicializar el socket del servidor
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(5)
-print(f"Servidor escuchando en {host}:{port}")
 
-# Aceptar conexiones entrantes
+print("Servidor ATM en ejecución...")
+
 while True:
     client_socket, client_address = server_socket.accept()
     print(f"Conexión entrante de {client_address}")
